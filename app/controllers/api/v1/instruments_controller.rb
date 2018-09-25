292134867afa49b68dd8371abf04a8bf7d361da0
JohnDaise <jdaise@gmail.com>
2018-09-25 16:00:56 -0400

    added update controller

--- app/controllers/api/v1/instruments_controller.rb
+++ app/controllers/api/v1/instruments_controller.rb
@@ -1,10 +1,22 @@
 class Api::V1::InstrumentsController < ApplicationController
+  before_action :find_instrument, only: [:update, :destroy]
+
+
   def index
     render json: Instrument.all
   end
 
   def show
     render json: Instrument.find(params[:id])
+  end
+
+  def update
+    @instrument.update(instrument_params)
+    if @instrument.save
+      render json: @instrument, status: :accepted
+    else
+      render json: { errors: @instrument.errors.full_messages }, status: :unprocessible_entity
+    end
   end
 
   private
